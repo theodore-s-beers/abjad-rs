850f5d3dbb672aa3afc9c2f0bd6aee30641550a1
Theo Beers <theo.beers@fu-berlin.de>
2021-04-28 15:01:54 +0200

    Flesh out functions and tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,75 +1,132 @@
+use anyhow::{anyhow, Result};
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
+    fn all() {
+        let input = "ابجد هوز حطي كلمن سعفص قرشت ثخذ ضظغ";
+
+        let prefs_mashriqi: AbjadPrefs = Default::default();
+        let prefs_maghribi = AbjadPrefs {
+            maghribi_order: true,
+            ..Default::default()
+        };
+
+        let total_mashriqi = abjad_strict(input, prefs_mashriqi).unwrap();
+        let total_maghribi = abjad_strict(input, prefs_maghribi).unwrap();
+
+        assert_eq!(total_mashriqi, 5995);
+        assert_eq!(total_mashriqi, total_maghribi);
+    }
+
+    #[test]
     fn baha_count() {
-        let input = "بهاء".to_string();
+        let input = "بهاء";
         let prefs: AbjadPrefs = Default::default();
 
-        assert_eq!(abjad(input, prefs), 9);
+        assert_eq!(abjad_strict(input, prefs).unwrap(), 9);
     }
 
     #[test]
     fn baha_ignore() {
-        let input = "بهاء".to_string();
+        let input = "بهاء";
         let prefs = AbjadPrefs {
             ignore_lone_hamzah: true,
             ..Default::default()
         };
 
-        assert_eq!(abjad(input, prefs), 8);
+        assert_eq!(abjad_strict(input, prefs).unwrap(), 8);
     }
 
     #[test]
     fn basmala() {
-        let input = "بسم الله الرحمن الرحيم".to_string();
+        let input = "بسم الله الرحمن الرحيم";
         let prefs: AbjadPrefs = Default::default();
 
-        assert_eq!(abjad(input, prefs), 786);
+        assert_eq!(abjad_strict(input, prefs).unwrap(), 786);
     }
 
     #[test]
     fn humayun() {
-        let input = "همایون پادشاه از بام افتاد".to_string();
+        let input = "همایون پادشاه از بام افتاد";
         let prefs: AbjadPrefs = Default::default();
 
-        assert_eq!(abjad(input, prefs), 962);
+        assert_eq!(abjad_strict(input, prefs).unwrap(), 962);
     }
 
     #[test]
     fn latin() {
-        let input = "the quick brown fox".to_string();
+        let input = "the quick brown fox";
         let prefs: AbjadPrefs = Default::default();
 
         assert_eq!(abjad(input, prefs), 0);
     }
 
     #[test]
+    fn latin_report() {
+        let input = "the quick brown fox";
+        let prefs: AbjadPrefs = Default::default();
+
+        let (total, errors) = abjad_collect_errors(input, prefs);
+
+        assert_eq!(total, 0);
+        assert_eq!(errors.len(), 16);
+    }
+
+    #[test]
     fn mixture() {
-        let input = "روح الله tapdancing خمینی".to_string();
+        let input = "روح الله tapdancing خمینی";
         let prefs: AbjadPrefs = Default::default();
 
         assert_eq!(abjad(input, prefs), 990);
     }
 
     #[test]
+    fn mixture_report() {
+        let input = "روح الله tapdancing خمینی";
+        let prefs: AbjadPrefs = Default::default();
+
+        let (total, errors) = abjad_collect_errors(input, prefs);
+
+        assert_eq!(total, 990);
+        assert_eq!(errors.len(), 10);
+    }
+
+    #[test]
     fn shaddah() {
-        let input = "رئیس مؤسّس دانشگاه".to_string();
+        let input = "رئیس مؤسّس دانشگاه";
         let prefs = AbjadPrefs {
             count_shaddah: true,
             ..Default::default()
         };
 
-        assert_eq!(abjad(input, prefs), 887);
+        assert_eq!(abjad_strict(input, prefs).unwrap(), 887);
+    }
+
+    #[test]
+    fn tammamtu() {
+        let input = "قد تمّمته";
+        let prefs: AbjadPrefs = Default::default();
+
+        assert_eq!(abjad_strict(input, prefs).unwrap(), 989);
     }
 
     #[test]
     fn vahshi() {
-        let input = "وفات وحشی مسکین".to_string();
+        let input = "وفات وحشی مسکین";
         let prefs: AbjadPrefs = Default::default();
 
-        assert_eq!(abjad(input, prefs), 991);
+        assert_eq!(abjad_strict(input, prefs).unwrap(), 991);
+    }
+
+    #[test]
+    fn zwnj() {
+        let input = "عادت می‌کنیم";
+        let prefs: AbjadPrefs = Default::default();
+
+        assert_eq!(abjad_strict(input, prefs).unwrap(), 645);
     }
 }
 
@@ -81,185 +138,196 @@ pub struct AbjadPrefs {
     pub maghribi_order: bool,
 }
 
-pub fn abjad(input: String, prefs: AbjadPrefs) -> u32 {
+pub fn abjad(input: &str, prefs: AbjadPrefs) -> u32 {
     let count_shaddah = prefs.count_shaddah;
     let double_alif_maddah = prefs.double_alif_maddah;
     let ignore_lone_hamzah = prefs.ignore_lone_hamzah;
     let maghribi_order = prefs.maghribi_order;
 
     let mut abjad_total: u32 = 0;
+
     let mut last_value: u32 = 0;
 
     for character in input.chars() {
-        match character {
-            'ا' | 'أ' | 'إ' | 'ٱ' => {
-                abjad_total += 1;
-                last_value = 1
-            }
-            'آ' => {
-                if double_alif_maddah {
-                    abjad_total += 2;
-                    last_value = 2
-                } else {
-                    abjad_total += 1;
-                    last_value = 1
-                }
-            }
-            'ء' => {
-                if ignore_lone_hamzah {
-                    last_value = 0
-                } else {
-                    abjad_total += 1;
-                    last_value = 1
-                }
-            }
-            'ب' | 'پ' => {
-                abjad_total += 2;
-                last_value = 2
-            }
-            'ج' | 'چ' => {
-                abjad_total += 3;
-                last_value = 3
-            }
-            'د' => {
-                abjad_total += 4;
-                last_value = 4
-            }
-            'ه' | 'ة' | 'ۀ' => {
-                abjad_total += 5;
-                last_value = 5
-            }
-            'و' | 'ؤ' => {
-                abjad_total += 6;
-                last_value = 6
-            }
-            'ز' | 'ژ' => {
-                abjad_total += 7;
-                last_value = 7
-            }
-            'ح' => {
-                abjad_total += 8;
-                last_value = 8
-            }
-            'ط' => {
-                abjad_total += 9;
-                last_value = 9
-            }
-            'ي' | 'ى' | 'ئ' | 'ی' => {
-                abjad_total += 10;
-                last_value = 10
-            }
-            'ك' | 'ک' | 'گ' => {
-                abjad_total += 20;
-                last_value = 20
-            }
-            'ل' => {
-                abjad_total += 30;
-                last_value = 30
-            }
-            'م' => {
-                abjad_total += 40;
-                last_value = 40
-            }
-            'ن' => {
-                abjad_total += 50;
-                last_value = 50
-            }
-            'س' => {
-                if maghribi_order {
-                    abjad_total += 300;
-                    last_value = 300
-                } else {
-                    abjad_total += 60;
-                    last_value = 60
-                }
-            }
-            'ع' => {
-                abjad_total += 70;
-                last_value = 70
-            }
-            'ف' => {
-                abjad_total += 80;
-                last_value = 80
-            }
-            'ص' => {
-                if maghribi_order {
-                    abjad_total += 60;
-                    last_value = 60
-                } else {
-                    abjad_total += 90;
-                    last_value = 90
-                }
-            }
-            'ق' => {
-                abjad_total += 100;
-                last_value = 100
-            }
-            'ر' => {
-                abjad_total += 200;
-                last_value = 200
-            }
-            'ش' => {
-                if maghribi_order {
-                    abjad_total += 1000;
-                    last_value = 1000
-                } else {
-                    abjad_total += 300;
-                    last_value = 300
-                }
-            }
-            'ت' => {
-                abjad_total += 400;
-                last_value = 400
-            }
-            'ث' => {
-                abjad_total += 500;
-                last_value = 500
-            }
-            'خ' => {
-                abjad_total += 600;
-                last_value = 600
-            }
-            'ذ' => {
-                abjad_total += 700;
-                last_value = 700
-            }
-            'ض' => {
-                if maghribi_order {
-                    abjad_total += 90;
-                    last_value = 90
-                } else {
-                    abjad_total += 800;
-                    last_value = 800
-                }
-            }
-            'ظ' => {
-                if maghribi_order {
-                    abjad_total += 800;
-                    last_value = 800
-                } else {
-                    abjad_total += 900;
-                    last_value = 900
-                }
-            }
-            'غ' => {
-                if maghribi_order {
-                    abjad_total += 900;
-                    last_value = 900
-                } else {
-                    abjad_total += 1000;
-                    last_value = 1000
-                }
-            }
-            'ّ' => {
-                if count_shaddah {
-                    abjad_total += last_value;
-                }
-                last_value = 0
-            }
-            _ => last_value = 0,
+        if let Ok(new_value) = get_letter_value(
+            character,
+            last_value,
+            count_shaddah,
+            double_alif_maddah,
+            ignore_lone_hamzah,
+            maghribi_order,
+        ) {
+            abjad_total += new_value;
+
+            last_value = new_value
+        } else {
+            last_value = 0
         }
     }
 
     abjad_total
+}
+
+pub fn abjad_collect_errors(input: &str, prefs: AbjadPrefs) -> (u32, Vec<String>) {
+    let count_shaddah = prefs.count_shaddah;
+    let double_alif_maddah = prefs.double_alif_maddah;
+    let ignore_lone_hamzah = prefs.ignore_lone_hamzah;
+    let maghribi_order = prefs.maghribi_order;
+
+    let mut abjad_total: u32 = 0;
+
+    let mut errors: Vec<String> = Vec::new();
+
+    let mut last_value: u32 = 0;
+
+    for character in input.chars() {
+        if let Ok(new_value) = get_letter_value(
+            character,
+            last_value,
+            count_shaddah,
+            double_alif_maddah,
+            ignore_lone_hamzah,
+            maghribi_order,
+        ) {
+            abjad_total += new_value;
+
+            last_value = new_value
+        } else {
+            errors.push(character.escape_unicode().collect());
+
+            last_value = 0
+        }
+    }
+
+    (abjad_total, errors)
+}
+
+pub fn abjad_strict(input: &str, prefs: AbjadPrefs) -> Result<u32> {
+    let count_shaddah = prefs.count_shaddah;
+    let double_alif_maddah = prefs.double_alif_maddah;
+    let ignore_lone_hamzah = prefs.ignore_lone_hamzah;
+    let maghribi_order = prefs.maghribi_order;
+
+    let mut abjad_total: u32 = 0;
+
+    let mut last_value: u32 = 0;
+
+    for character in input.chars() {
+        let new_value = get_letter_value(
+            character,
+            last_value,
+            count_shaddah,
+            double_alif_maddah,
+            ignore_lone_hamzah,
+            maghribi_order,
+        )?;
+
+        abjad_total += new_value;
+
+        last_value = new_value
+    }
+
+    Ok(abjad_total)
+}
+
+fn get_letter_value(
+    character: char,
+    last_value: u32,
+    count_shaddah: bool,
+    double_alif_maddah: bool,
+    ignore_lone_hamzah: bool,
+    maghribi_order: bool,
+) -> Result<u32> {
+    let mut letter_value: u32 = 0;
+
+    match character {
+        'ا' | 'أ' | 'إ' | 'ٱ' => letter_value = 1,
+        'آ' => {
+            if double_alif_maddah {
+                letter_value = 2
+            } else {
+                letter_value = 1
+            }
+        }
+        'ء' => {
+            if !ignore_lone_hamzah {
+                letter_value = 1
+            }
+        }
+        'ب' | 'پ' => letter_value = 2,
+        'ج' | 'چ' => letter_value = 3,
+        'د' => letter_value = 4,
+        'ه' | 'ة' | 'ۀ' => letter_value = 5,
+        'و' | 'ؤ' => letter_value = 6,
+        'ز' | 'ژ' => letter_value = 7,
+        'ح' => letter_value = 8,
+        'ط' => letter_value = 9,
+        'ي' | 'ى' | 'ئ' | 'ی' => letter_value = 10,
+        'ك' | 'ک' | 'گ' => letter_value = 20,
+        'ل' => letter_value = 30,
+        'م' => letter_value = 40,
+        'ن' => letter_value = 50,
+        'س' => {
+            if maghribi_order {
+                letter_value = 300;
+            } else {
+                letter_value = 60;
+            }
+        }
+        'ع' => letter_value = 70,
+        'ف' => letter_value = 80,
+        'ص' => {
+            if maghribi_order {
+                letter_value = 60;
+            } else {
+                letter_value = 90;
+            }
+        }
+        'ق' => letter_value = 100,
+        'ر' => letter_value = 200,
+        'ش' => {
+            if maghribi_order {
+                letter_value = 1000
+            } else {
+                letter_value = 300
+            }
+        }
+        'ت' => letter_value = 400,
+        'ث' => letter_value = 500,
+        'خ' => letter_value = 600,
+        'ذ' => letter_value = 700,
+        'ض' => {
+            if maghribi_order {
+                letter_value = 90
+            } else {
+                letter_value = 800
+            }
+        }
+        'ظ' => {
+            if maghribi_order {
+                letter_value = 800
+            } else {
+                letter_value = 900
+            }
+        }
+        'غ' => {
+            if maghribi_order {
+                letter_value = 900
+            } else {
+                letter_value = 1000
+            }
+        }
+        // Next is the shaddah diacritic; will probably look funny
+        'ّ' => {
+            if count_shaddah {
+                letter_value = last_value;
+            }
+        }
+        // Space or zwnj is ok
+        ' ' | '‌' => {}
+        // Otherwise return error
+        _ => return Err(anyhow!("Unrecognized character")),
+    }
+
+    Ok(letter_value)
 }
